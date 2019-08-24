2eef6a33af0fea68cd23297a7c9e13b708860bcc
Olga804 <54376627+Olga804@users.noreply.github.com>
2019-08-24 21:30:53 +0300

    24/08/2019/2130

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Collection;
 import java.util.Map;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
@@ -6,33 +7,35 @@ import java.util.TreeMap;
 /*
 Делает: открывает файл; считывает значения; сортирует по порядку;
 считает, сколько раз повторяется слово;выводит слово, встречающееся
-максимальное количество раз и его частоту повторений, если таких слов несколько, выводит все;
-предусмотрен ввод абсолютного пути к файлу; работает только с латиницей;
+максимальное количество раз  его частоту, если таких слов несколько, выводит все;
 
-Не делает: предусмотреть ввод относительного пути к файлу; ввод русских букв в файле;
+Не делает: не работает с русским текстом;
  */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-       Scanner scanner=new Scanner(new File("C:\\test.txt"));
+       Scanner scanner=new Scanner(new File("test.txt"));
        Map<String,Integer>statistics=new TreeMap<>();
+       Collection<String>collection=statistics.keySet();
 
 
-
+       Integer max=0;
+       double all=0;
        while(scanner.hasNext()){
-           String word=scanner.useDelimiter("\\s+").next();
+           String word=scanner.useDelimiter("\\s+|,+|\\.+|;+|'+|\"+").next();
            Integer cout=statistics.get(word);
            if(cout==null){
                cout=0;
            }
            statistics.put(word, ++cout);
-             if (statistics.get(word)>max){
+           all++;
+
+           if (statistics.get(word)>max){
                max=statistics.get(word);
            }
            }
        for (Map.Entry item:
                 statistics.entrySet()) {
             System.out.println(item);
-
        }
         System.out.println("Maximum:");
 
@@ -41,11 +44,16 @@ public class Main {
             Integer i=statistics.get(key);
             if(max.equals(i)){
                 System.out.print(key+" ");
-                System.out.println(max);
-                System.out.println(max/statistics.size());
-            }
+                System.out.println("repeat "+max+" times");
+                System.out.println("frequency:"+max/all);
 
         }
+
+
+        }
+
+
+
 
     }
 
